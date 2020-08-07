0944548f9364312008bc5d6111d9f27e0bf77afb
Susannah <susannah@pobox.com>
2020-08-07 09:17:47 -0700

    plot ypred and y

--- ppc.R
+++ ppc.R
@@ -2,8 +2,19 @@
 # 
 # 
 library(tidybayes)
+library(dplyr)
 
 fit <- readRDS("2020-08-06sypcFEMALE.rds") %>%
-  tidybayes::recover_types()
+  tidybayes::recover_types(phenfs)
+
+  
+ypred <- fit %>%
+  spread_draws(y_ppc[i], n=100, seed=87)
+
+ggplot(ypred, aes(x=y_ppc, color="model") ) +
+  geom_density() +
+  geom_density(data=phenfs, aes(x=sum_forcing, color="observations"), inherit.aes = FALSE) +
+  theme_classic(base_size = 18) +
+  xlab("accumulated forcing")
 
 
