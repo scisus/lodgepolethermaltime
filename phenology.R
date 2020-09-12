5da445cee38f4ecc6c1610d3528942dd7b10f423
Susannah <susannah@pobox.com>
2020-09-12 15:29:36 -0700

    Move functions to separate file

--- phenology.R
+++ phenology.R
@@ -8,76 +8,18 @@ library(tidybayes)
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
+source('phenology_functions.R')
 
-phen <- flowers::phenology %>% # phenology data
-    filter(Phenophase_Derived==2 & Year < 2012) %>% # need to remake all_clim_PCIC.csv to include 2012
-    rename(state = Phenophase_Derived) # only consider days when trees are flowering
+# choose only phenology data that is the start or end date
+phenbe <- filter_start_end()
 
-forcing <- read.csv("../phenolology/data/all_clim_PCIC.csv", header=TRUE, stringsAsFactors = FALSE) %>%
-    filter(forcing_type=="ristos") # only consider forcing units calculated based on work of Sarvas 1972
-
-spus <- read.csv("../phd/data/OrchardInfo/LodgepoleSPUs.csv") %>%
-    select(SPU_Name, Orchard) # provenance information for each orchard in phen
-
-# filter phenology dataset to only include start and end phenology
-phenbe <- dplyr::filter(phen, DoY == First_RF | DoY == Last_RF) %>%
-    dplyr::left_join(forcing) %>%
-    dplyr::left_join(spus) %>%
-    dplyr::mutate(Year = as.character(Year), Clone = as.character(Clone)) %>%
-    dplyr::rename(Provenance = SPU_Name)
-
-
-# Fit a model in Stan to phenology data, return the model fit object and save the model fit object to a file. Choose whether the model is for "MALE" or "FEMALE" strobili and whether the event is the "begin" or "end" of flowering. data is a dataframe of flowering data. id is an optional identifier appended to the file name.
-fit_model <- function(data, sex, event, model = "phenology.stan", maxtreedepth=10) {
-    
-    # receptivity start
-    phensub <- data %>% 
-        dplyr::filter(if (event == "begin") Sex == sex & DoY == First_RF else Sex == sex & DoY == Last_RF) %>%
-        dplyr::select(sum_forcing, Site, Year, Provenance, Clone)
-    
-    # prepare data for stan
-    
-    input <- tidybayes::compose_data(phensub)
-    
-    # add event-specific prior
-    if (event == "begin") {
-        input <- c(input, mu_mean=335, mu_sigma = 50)
-    } 
-    
-    if (event == "end") {
-        input <- c(input, mu_mean=555, mu_sigma = 90)
-    } 
-    
-    
-    fit <- rstan::stan(file= model, chains=8, data=input, iter=3500, cores=9, 
-                       pars=c("z_clone_alpha", "z_year_alpha"), include=FALSE, 
-                       init = rep(list(list(mu = abs(rnorm(1,100,50)), 
-                                            sigma = rexp(1,1), 
-                                            sigma_site = rexp(1,1), 
-                                            sigma_year = rexp(1,1), 
-                                            sigma_prov = rexp(1,1), 
-                                            sigma_clone = rexp(1,1))), 8),
-                       control = list(max_treedepth = maxtreedepth, adapt_delta=0.9))
-    
-    gc()
-    
-    saveRDS(fit, file = paste(Sys.Date(), sex, "_", event, ".rds", sep=''))
-    
-    return(fit)
-}
-
+# fit models
 female_begin <- fit_model(data = phenbe, sex = "FEMALE", event = "begin")
 female_end <- fit_model(data = phenbe, sex = "FEMALE", event = "end")
 
 male_begin <- fit_model(data=phenbe, sex="MALE", event = "begin")
 male_end <- fit_model(data = phenbe, sex="MALE", event = "end")
 
-# fitvars <- as.data.frame(rstan::extract(fit))
-# 
-# Rhat()
-# ess_bulk()
-# ess_tail()
-# launch_shinystan(fit)
 
 # plot modeled and true data
 
