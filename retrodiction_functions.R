010d8d125e73408444ffed9ba54753cc30f66398
Susannah <susannah@pobox.com>
2021-01-20 10:10:52 -0800

    write a function for building a retrodiction dataframe

--- retrodiction_functions.R
+++ retrodiction_functions.R
@@ -15,22 +15,6 @@ split_df_to_lists <- function(a, b) {
   return(list(listainb = ainb, listb = listb))
 }
 
-# # given dataframes a (climate) and b (phenology) identify the day of year in a corresponding to reaching each sum_forcing in b. a must have a sum_forcing column and a DoY column and b must have a sum_forcing column. name the new_day_col arg with a string
-# find_day_of_forcing <- function(a, b, new_doy_col) {
-#   
-#   # what row in a contains the interval for entries in b. Add 1 to the index because phenological events require the threshold to be reached. this introduces error, but is unavoidable in some direction.
-#   a_index <- findInterval(b$sum_forcing, a$sum_forcing) + 1
-#   
-#   
-#   # add a column to b for Day of Year and extract the correct Day of year from a using the index
-#   b[new_doy_col] <- a$DoY[a_index] 
-#   
-#   # when sum_forcing in b is exactly identical to sum_forcing in b, a_index will be +1 day. Re-write those 
-#   identical_forcing_index <- which(b$sum_forcing %in% a$sum_forcing)
-#   b[[new_doy_col]][identical_forcing_index] <- b[[new_doy_col]][identical_forcing_index] - 1
-#   
-#   return(b)
-# }
 
 # given dataframes adf (climate) and bdf (phenology) identify the day of year corresponding to reaching each sum_forcing bcol in df. adf must have a sum_forcing column identified by name acol and a DoY column and b must have a sum_forcing column identified with name bcol. name the new_day_col arg with a string
 find_day_of_forcing <- function(adf, bdf, aforce, bforce) {
@@ -50,7 +34,7 @@ find_day_of_forcing <- function(adf, bdf, aforce, bforce) {
 }
 
 
-# find DoY from a climate dataset a corresponding to each sum_forcing in a phenology dataset b. Identify column names of forcing columns as strings aforce and bforce and the name of the new doy column as a string to new_doy_col. Day of Year in the climate dataset must be DoY
+# find DoY from a climate dataset a corresponding to each sum_forcing in a phenology dataset b. Identify column names of forcing columns as strings aforce and bforce and the name of the new doy column as a string to newdoycol. Day of Year in the climate dataset must be DoY
 forcing_to_doy <- function(a, b, aforce, bforce) {
   # prepare dataframes for interval finding by splitting into lists
   splitdfs <- split_df_to_lists(a, b)
@@ -59,4 +43,48 @@ forcing_to_doy <- function(a, b, aforce, bforce) {
     purrr::map_dfr(bind_rows) # combine into a single dataframe
   
   return(df$newdoycol)
+}
+
+# build a dataframe of retrodictions for sum_forcing and doy with associate Site, Provenance, Clone and Year information. Requires model file with sum_forcing_rep parameter and data used in the model as well as daily temperature with sum_forcing for sites and years in the data.
+retrodict <- function(modelfile, dat, climate) {
+  fit <- readRDS(modelfile)
+  
+  fit %<>% recover_types(dat)
+  
+  rep <- fit %>%
+    #tidybayes::spread_draws(`sum_forcing_rep.*`[i], regex=TRUE, n=n, seed=seed) %>% # y_ppc generated in stan model into tidy df
+    tidybayes::spread_draws(`sum_forcing_rep.*`[i], regex=TRUE) %>% # y_ppc generated in stan model into tidy df
+    dplyr::left_join(
+      dplyr::select(dat, Site, Year, i) # add identifying information (Site, Year) from data for matching with climate
+    ) 
+  
+  # convert forcing units to day of year
+  rep$doy_rep <- forcing_to_doy(a = climate, b = data.frame(rep), aforce = "sum_forcing", bforce = "sum_forcing_rep") 
+  
+  retrodiction <- dplyr::left_join(dat, rep) # dataframe with observed and modeled sum_forcing and DoY
+  
+  return(retrodiction)
+}
+
+# calculate HPDIs for each group, determine Day of Year associated with HPDIs, and calculate whether group observations are within intervals for day and sum_forcing
+intervalate <- function(retrodictions, climate, dat) {
+  intervals <- retrodictions %>%
+    group_by(i) %>%
+    median_hdi(sum_forcing_rep, .width=c(0.50, 0.75, 0.90)) %>%
+    rename(sum_forcing_rep_median = sum_forcing_rep) %>%
+    full_join(dat) 
+  
+  # what DoY is associated with each forcing?
+  intervals$.lower_doy <- forcing_to_doy(a = climate, b = intervals, aforce = "sum_forcing", bforce = ".lower") 
+  intervals$.upper_doy <- forcing_to_doy(a = climate, b = intervals, aforce = "sum_forcing", bforce = ".upper") 
+  intervals$doy_rep_median <- forcing_to_doy(a = climate, b = intervals, aforce = "sum_forcing", bforce = "sum_forcing_rep_median") 
+  
+  # What proportion of observations are within the HDPIs?
+  intervals <- intervals %>%
+    dplyr::mutate(in_forcing_int = case_when(sum_forcing >= .lower & sum_forcing <= .upper ~ TRUE,
+                                             sum_forcing < .lower | sum_forcing > .upper ~ FALSE),
+                  in_doy_int = case_when(DoY >= .lower_doy & DoY <= .upper_doy ~ TRUE,
+                                         DoY < .lower_doy | DoY > .upper_doy ~ FALSE))
+  
+  return(intervals)
 }
