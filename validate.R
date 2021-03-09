1e0f610e2744c6f067005098bee052bdd76ab89c
Susannah <susannah@pobox.com>
2021-03-09 13:18:45 -0800

    calculate loo stats for 3 models

--- validate.R
+++ validate.R
@@ -7,32 +7,53 @@ library(dplyr)
 source('phenology_functions.R')
 
 # phenology data
-# 
+
 phenbe <- filter_start_end()
-dat <- select_data(phenbe, "FEMALE", "begin", keep_day = TRUE) 
-dat$i <- 1:nrow(dat)
+ristodat <- select_data(phenbe, "FEMALE", "begin", keep_day = TRUE) 
+ristodat$i <- 1:nrow(dat)
 
-fbfitcombined <- readRDS('2021-03-03FEMALE_begin.rds')
+# add gdd data
 
-llsiteyear <- loo::extract_log_lik(fbfitcombined) # pull log likelihoods for waic & loo calculations
+#dat$sum_forcing_gdd <-# 
+dat <- filter_start_end(forcingname = "gdd") %>%
+  select_data("FEMALE", "begin", keep_day = TRUE) %>%
+  mutate(i = 1:n()) %>%
+  rename(gdd = sum_forcing) %>%
+  full_join(ristodat) %>%
+  rename(ristos = sum_forcing)
+  
+  
 
-loo::waic(llsiteyear)
+fbfitcombined <- readRDS('2021-03-03FEMALE_beginSY.rds')
+fbfitseparate <- readRDS('2021-03-09FEMALE_begin.rds')
+fbfitgdd <- readRDS('2021-03-09FEMALE_begin_gdd.rds')
 
-loosy <- loo::loo(llsiteyear2, r_eff = relative_eff(exp(llsiteyear2)), save_psis = TRUE, cores = 10)
-plot(loosy)
+llcombined <- loo::extract_log_lik(fbfitcombined, merge_chains = FALSE) # pull log likelihoods for waic & loo calculations
+llseparate <- loo::extract_log_lik(fbfitseparate, merge_chains = FALSE)
+llgdd <- loo::extract_log_lik(fbfitgdd, merge_chains = FALSE)
 
-bad <- which(loosy$diagnostics$pareto_k >= 0.7)
-ok <- which(loosy$diagnostics$pareto_k < 0.7 & loosy$diagnostics$pareto_k > 0.5)
+# use loo instead, too many p_waic estimates > 0.5
+# loo::waic(llcombined) 
+# loo::waic(llseparate)
+# loo::waic(loo::extract_log_lik(fbfitgdd))
 
-dat$paretok <-loosy$diagnostics$pareto_k
+loocombined <- loo::loo(llcombined, r_eff = relative_eff(exp(llcombined)), save_psis = TRUE, cores = 10)
+looseparate <- loo::loo(llseparate, r_eff = relative_eff(exp(llseparate)), save_psis = TRUE, cores = 10)
+loogdd <- loo::loo(llgdd, r_eff = relative_eff(exp(llgdd)), save_psis = TRUE, cores = 10)
 
+loo_compare(loocombined, looseparate, loogdd)
 
-ggplot(dat, aes(x=i, y=paretok, colour = Site)) +
+dat$paretokcombined <-loocombined$diagnostics$pareto_k
+dat$paretoseparate <- looseparate$diagnostics$pareto_k
+dat$paretogdd <- loogdd$diagnostics$pareto_k
+
+loodat <- pivot_longer(dat, starts_with("pareto"), names_to = "model", values_to = "pareto_k") %>%
+  pivot_longer(cols = c("gdd", "ristos"), names_to = "forcingtype", values_to = "sum_forcing")
+
+ggplot(loodat, aes(x=i, y=pareto_k, colour = Site)) +
   geom_point(shape = 3) +
-  geom_hline(yintercept = c(0.5, 0.7), lty=2, colour = "darkgray")
+  geom_hline(yintercept = c(0.5, 0.7), lty=2, colour = "darkgray") +
+  facet_wrap("model")
 
-ggplot(dat, aes(x=i, y=paretok, colour = Provenance)) +
-  geom_point(shape = 3) +
-  geom_hline(yintercept = c(0.5, 0.7), lty=2, colour = "darkgray")
 
 
