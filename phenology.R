d47b30f31c594daff2314838237c21a1628369c2
Susannah <susannah@pobox.com>
2021-05-10 12:10:43 -0700

    evaluate problems with phenology model and dataset

--- phenology.R
+++ phenology.R
@@ -11,8 +11,31 @@ options(mc.cores = parallel::detectCores())
 #source('calculate_forcingunits.R')
 source('phenology_functions.R')
 
+phenraw <- flowers::phenology %>%
+  #filter(Site == "Kalamalka", Sex == "FEMALE", Year == 2007, Orchard == 230, Clone == 1460)
+  filter(Phenophase == "receptive80")
+
+phen <- filter_start_end() #%>%
+  dplyr::mutate(event = case_when(DoY == First_RF ~ "begin",
+                                  DoY == Last_RF ~ "end"))
+
+foo <- phen %>% filter(Site != "PGTIS", DoY == Last_RF)
+
+foo %>% filter(First_RF != Last_RF)
+
+function(phendat, censordat = NULL, factors, sex, event, keep_day = FALSE)
+foo <- select_data(phendat = phen, censordat = NULL, factors = c("Site", "Year", "Provenance", "Clone"), sex = "FEMALE", event = "end")
+
+ggplot(phen, aes(x = sum_forcing, colour = Site)) +
+  stat_ecdf() +
+  facet_grid(Sex ~ event)
+
 
 censorbegin <- add_censor_indicator()
+
+censorbegin %>%
+  group_by(Site, Sex) %>%
+  summarise(percent_censored = sum(100*censored)/n())
 
 phenbe <- filter_start_end()
 
@@ -36,17 +59,35 @@ init <- rep(list(list(mu = abs(rnorm(1,100,50)),
 
 # options for censored data models
 # warmup = 1500, iter = 4500, control = list(max_treedepth = 12),
+#
+
+# library(ggbeeswarm)
+# ggplot(fbdat, aes(x = Site, y=sum_forcing, colour = as.factor(censored))) +
+#   geom_quasirandom(alpha = 0.5)
+#
+# ggplot(fbdat, aes(x = as.factor(censored), y = sum_forcing)) +
+#   geom_quasirandom(alpha = 0.5) +
+#   facet_wrap("Site")
+#
+# ggplot(mbdat, aes(x = as.factor(censored), y = sum_forcing)) +
+#   geom_quasirandom(alpha = 0.5) +
+#   facet_wrap("Site")
+
+
+
+
 
 # fit models
-fbdat <- select_data(phenbe, censordat = censorbegin, factors = factors, sex = "FEMALE", event = "begin")
-dat <- select_data(phenbe, factors = factors, sex = "FEMALE", event = "begin")
-fbinput <- prepare_data_for_stan(fbdat, factor_threshold_list = factor_threshold_list, event = "begin")
-fbfit <- sample_stan_model(phenologymodel, input = fbinput, sex = "FEMALE", event = "begin", expars=expars)
+# fbdat <- select_data(phenbe, censordat = censorbegin, factors = factors, sex = "FEMALE", event = "begin")
+# mbdat <- select_data(phenbe, censordat = censorbegin, factors = factors, sex = "MALE", event = "begin")
+# dat <- select_data(phenbe, factors = factors, sex = "FEMALE", event = "begin")
+# fbinput <- prepare_data_for_stan(fbdat, factor_threshold_list = factor_threshold_list, event = "begin")
+# fbfit <- sample_stan_model(phenologymodel, input = fbinput, sex = "FEMALE", event = "begin", expars=expars)
 #, warmup = 1500, iter = 4500, control = list(max_treedepth = 12),
 
 female_begin <- munge_and_fit(phendat = phenbe, censordat = censorbegin, sex = "FEMALE", event = "begin", appendname = "_censored", compiledmodel = phenologymodel,
                               factors = factors, factor_threshold_list = factor_threshold_list,
-                              expars = expars, init = init, control = list(max_treedepth = 11), iter = 4500, warmup = 2000)
+                              expars = expars, init = init, control = list(adapt_delta = 0.9))#, control = list(max_treedepth = 11), iter = 4500, warmup = 2000)
 
 #female_begin <- fit_model(phendat = phenbe, sex = "FEMALE", censorship = censorbegin, event = "begin", maxtreedepth = 12, iter = 4500, warmup = 1500)
 #male_begin <- fit_model(phendat = phenbe, sex = "MALE", censorship = censorbegin, event = "begin", maxtreedepth = 12, iter = 4500, warmup = 1500)
@@ -56,9 +97,9 @@ female_end <- munge_and_fit(phendat = phenbe, sex = "FEMALE", event = "end", com
                             factors = factors, factor_threshold_list = factor_threshold_list,
                             expars = expars, init = init)
 
-male_begin <- munge_and_fit(phendat = phenbe, sex = "MALE", event = "begin", compiledmodel = phenologymodel,
+male_begin <- munge_and_fit(phendat = phenbe, censordat = censorbegin, sex = "MALE", event = "begin", compiledmodel = phenologymodel, appendname = "_censored",
                             factors = factors, factor_threshold_list = factor_threshold_list,
-                            expars = expars, init = init)
+                            expars = expars, init = init, control = list(adapt_delta = 0.95))
 
 male_end <- munge_and_fit(phendat = phenbe, sex = "MALE", event = "end", compiledmodel = phenologymodel,
                           factors = factors, factor_threshold_list = factor_threshold_list,
