970b60322f749deb22349d6eec5ce034e60d4096
Susannah <susannah@pobox.com>
2021-05-10 12:09:17 -0700

    check more versions of the model

--- diagnostics.R
+++ diagnostics.R
@@ -4,8 +4,7 @@ library(dplyr)
 library(purrr)
 library(rstan)
 
-fbfit <- readRDS('2021-04-29FEMALE_begin.rds')
-fbfitsy <- readRDS('2021-03-03FEMALE_beginSY.rds')
+fbfit <- readRDS('2021-05-09FEMALE_begin_censored.rds')
 fefit <- readRDS('2021-01-07FEMALE_end.rds')
 mbfit <- readRDS('2021-04-29MALE_begin_censored.rds')
 mefit <- readRDS('2021-01-07MALE_end.rds')
@@ -13,10 +12,12 @@ mefit <- readRDS('2021-01-07MALE_end.rds')
 #fbpars <- data.frame(rstan::extract(fbfit))
 
 
-fb <- readRDS("2021-05-06FEMALE_begin.rds")
-nuts <- bayesplot::nuts_params(fb)
-draws <- as.array(fb)
-#bayesplot::mcmc_parcoord(draws, pars = vars("mu", "sigma", starts_with("mu_"), starts_with("sigma_"), contains("alpha_site")), np=nuts, transform = function(x) {(x - mean(x)) / sd(x)})
+pairs(fbfit, pars = c("mu", "sigma", "mu_site", "sigma_site", "mu_year", "sigma_year", "mu_clone", "sigma_clone", "mu_prov", "sigma_prov"))
+pairs(fbfit, pars = c("mu", "sigma", "sigma_site", "sigma_year", "sigma_clone", "sigma_prov"))
+pairs(fbfit, pars = c("mu", "sigma", "mu_site", "mu_year", "mu_clone", "mu_prov"))
+nuts <- bayesplot::nuts_params(fbfit)
+draws <- as.array(fbfit)
+bayesplot::mcmc_parcoord(draws, pars = vars("mu", "sigma", starts_with("mu_"), starts_with("sigma_"), contains("alpha_site")), np=nuts, transform = function(x) {(x - mean(x)) / sd(x)})
 bayesplot::mcmc_intervals(draws, pars = vars(starts_with("mu_"), starts_with("sigma_"), contains("delta_site"), "sigma"))
 bayesplot::mcmc_intervals(draws, pars = vars( contains("year")))
 bayesplot::mcmc_areas(draws, pars = vars(contains("sigma_")))
@@ -24,7 +25,7 @@ bayesplot::mcmc_areas(draws, pars = vars(contains("mu_")))
 #female_begin_censored <- fit_model(phendat = censored, sex = "FEMALE", event = "begin")
 
 library(shinystan)
-launch_shinystan(fb)
+launch_shinystan(fbfit)
 
 
  # format for Rhat and ESS calculations
