a76b63ebe99d87de10b2a1d8965d8c71a61426d1
Susannah <susannah@pobox.com>
2021-10-14 07:44:23 -0700

    rewrite function do do correlations instead of lm

--- parclimphencorr.R
+++ parclimphencorr.R
@@ -32,11 +32,11 @@ phendat <- flowers::lodgepole_phenology_event %>%
   mutate(Tree = paste0(Orchard, Clone, X, Y)) # create a unique Tree identifier since original data doesn't always have one
 #phenf <- prepare_data(phendat = phendat, clim = histclim, spu = spudat)
 alldat <- readRDS("objects/alldat.rds") %>%
-  unite(fullevent, Sex, event, remove = FALSE) %>%
-  mutate_at("sum_forcing", ~(scale(.) %>% as.vector))
+  unite(fullevent, Sex, event, remove = FALSE)
 
 phenclim <- left_join(alldat, parentclim) %>%
-  filter(! is.na(.value))
+  filter(! is.na(.value)) %>%
+  rename(modelname = fullevent)
 
 #note that not all climate variables are equally good
 ns <- phenclim %>% group_by(.variable) %>%
@@ -48,79 +48,110 @@ ggplot(aes(y=.variable, x=n)) +
   geom_point() +
   ggtitle("Number of unique values per variable")
 
-model_function <- function(model.abb, variable){
-
-  model <- lm(sum_forcing ~ .value + Clone + Site + Year, na.action = na.omit,
-              data=phenclim %>% filter(fullevent == model.abb, .variable == variable))
-
-  output <- tidy(model, conf.int = TRUE) %>%
-    select(term, estimate, std.error, starts_with("conf")) %>%
-    mutate(event = model.abb, variable = variable)
-
-  metaout <- glance(model) %>%
-    mutate(event = model.abb, variable = variable)
-
-  return(list(pars = output, meta = metaout))
-}
+# model_function <- function(model.abb, variable){
+#
+#   model <- lm(sum_forcing ~ .value + Clone + Site + Year, na.action = na.omit,
+#               data=phenclim %>% filter(fullevent == model.abb, .variable == variable))
+#
+#   output <- tidy(model, conf.int = TRUE) %>%
+#     select(term, estimate, std.error, starts_with("conf")) %>%
+#     mutate(event = model.abb, variable = variable)
+#
+#   metaout <- glance(model) %>%
+#     mutate(event = model.abb, variable = variable)
+#
+#   return(list(pars = output, meta = metaout))
+# }
 
 # create a dataframe of events and variables
-modelplusvar <- purrr::cross_df(list(model_name = unique(phenclim$fullevent), variable = unique(phenclim$.variable)))
-result_df <- map2_df(.x = modelplusvar$model_name, .y=modelplusvar$variable, .f=model_function)
+modelplusvar <- purrr::cross_df(list(model_name = unique(phenclim$modelname), variable = unique(phenclim$.variable)))
+# result_df <- map2_df(.x = modelplusvar$model_name, .y=modelplusvar$variable, .f=model_function)
+#
+# result_df$pars %>%
+#   rename(model = event) %>%
+#   filter(term == ".value") %>%
+#   select(-term) %>%
+#   rename(term = variable) %>%
+#   filter(term %in% colnames(parentclim)) %>%
+#   arrange(estimate) %>%
+#   filter(term == "DD18_at") %>%
+#   dwplot() +
+#   theme_minimal() +
+#   geom_vline(xintercept = 0, alpha=.5, linetype=2) +
+#   theme(legend.position = 'top') +
+#   xlab("Estimate") + ylab("")
+#
+# result_df$meta %>%
+#   distinct() %>%
+#   rename(model = event) %>%
+#   arrange(r.squared) %>%
+#   ggplot(aes(x = r.squared, y = variable, colour = model)) +
+#   geom_point() +
+#   theme_minimal() +
+#   geom_vline(xintercept = 0, alpha=.5, linetype=2) +
+#   theme(legend.position = 'top') +
+#   xlab("Correlation") + ylab("") +
+#   facet_wrap("model", dir = "v")
+#
+# correlations <- result_df$meta %>% distinct() %>% arrange(desc(r.squared), event)
+# aic <- result_df$meta %>% distinct() %>% arrange(AIC)
+# meancorr <- result_df$meta %>%
+#   group_by(variable) %>%
+#   summarise(meanrsquared = mean(r.squared)) %>%
+#   arrange(desc(meanrsquared))
+#
+# effects <- result_df$pars %>% distinct() %>% arrange(desc(abs(estimate)), event)
+# meaneff <- result_df$pars %>%
+#   group_by(variable) %>%
+#   summarise(meaneffect = mean(estimate)) %>%
+#   arrange(desc(abs(meaneffect)))
+#
+# beginmeaneff <- result_df$pars %>%
+#   filter(event %in% c("FEMALE_begin", "MALE_begin")) %>%
+#   group_by(variable) %>%
+#   summarise(meaneffect = mean(estimate)) %>%
+#   arrange(desc(abs(meaneffect)))
+#
+# beginmeancorr <- result_df$meta %>%
+#   filter(event %in% c("FEMALE_begin", "MALE_begin")) %>%
+#   group_by(variable) %>%
+#   summarise(meanrsquared = mean(r.squared)) %>%
+#   arrange(desc(meanrsquared)) %>%
+#   filter(meanrsquared > 1.5e-2)
+#
+# ggplot(filter(phenclim, .variable == "MAT"), aes(x=.value, y=sum_forcing)) +
+#   geom_point()
+#
+# parentclim[, c(246:ncol(parentclim))] %>%
+#   pairs()
 
-result_df$pars %>%
-  rename(model = event) %>%
-  filter(term == ".value") %>%
-  select(-term) %>%
-  rename(term = variable) %>%
-  filter(term %in% colnames(parentclim)) %>%
+corr_function <- function(model.abb, variable){
+
+  dat <- filter(phenclim, modelname == model.abb, .variable == variable)
+  corr <- cor(dat$sum_forcing, dat$.value)
+
+
+  # output <- tidy(model, conf.int = TRUE) %>%
+  #   select(term, estimate, std.error, starts_with("conf")) %>%
+  #   mutate(modelname = model.abb, variable = variable)
+  #
+  # metaout <- glance(model) %>%
+  #   mutate(modelname = model.abb, variable = variable)
+  #
+  # return(list(pars = output, meta = metaout))
+  return(list(model_name = model.abb, variable = variable, correlation = corr))
+}
+
+corr_df <- map2_df(.x = modelplusvar$model_name, .y=modelplusvar$variable, .f=corr_function)
+
+corr_df %>%
+  rename(model = model_name) %>%
+  #filter(term == ".value") %>%
+  #select(-term) %>%
+  rename(term = variable, estimate = correlation) %>%
   arrange(estimate) %>%
-  filter(term == "DD18_at") %>%
   dwplot() +
   theme_minimal() +
   geom_vline(xintercept = 0, alpha=.5, linetype=2) +
   theme(legend.position = 'top') +
-  xlab("Estimate") + ylab("")
-
-result_df$meta %>%
-  distinct() %>%
-  rename(model = event) %>%
-  arrange(r.squared) %>%
-  ggplot(aes(x = r.squared, y = variable, colour = model)) +
-  geom_point() +
-  theme_minimal() +
-  geom_vline(xintercept = 0, alpha=.5, linetype=2) +
-  theme(legend.position = 'top') +
-  xlab("Correlation") + ylab("") +
-  facet_wrap("model", dir = "v")
-
-correlations <- result_df$meta %>% distinct() %>% arrange(desc(r.squared), event)
-aic <- result_df$meta %>% distinct() %>% arrange(AIC)
-meancorr <- result_df$meta %>%
-  group_by(variable) %>%
-  summarise(meanrsquared = mean(r.squared)) %>%
-  arrange(desc(meanrsquared))
-
-effects <- result_df$pars %>% distinct() %>% arrange(desc(abs(estimate)), event)
-meaneff <- result_df$pars %>%
-  group_by(variable) %>%
-  summarise(meaneffect = mean(estimate)) %>%
-  arrange(desc(abs(meaneffect)))
-
-beginmeaneff <- result_df$pars %>%
-  filter(event %in% c("FEMALE_begin", "MALE_begin")) %>%
-  group_by(variable) %>%
-  summarise(meaneffect = mean(estimate)) %>%
-  arrange(desc(abs(meaneffect)))
-
-beginmeancorr <- result_df$meta %>%
-  filter(event %in% c("FEMALE_begin", "MALE_begin")) %>%
-  group_by(variable) %>%
-  summarise(meanrsquared = mean(r.squared)) %>%
-  arrange(desc(meanrsquared)) %>%
-  filter(meanrsquared > 1.5e-2)
-
-ggplot(filter(phenclim, .variable == "MAT"), aes(x=.value, y=sum_forcing)) +
-  geom_point()
-
-parentclim[, c(246:ncol(parentclim))] %>%
-  pairs()
+  xlab("Pearson correlation") + ylab("")
