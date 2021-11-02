7c8b3bef4f6e0de7fabc1963ccf2fb1e6e022270
Susannah <susannah@pobox.com>
2021-11-01 17:50:11 -0700

    start thinking about how to explain countergradient variation

--- gradientoy.R
+++ gradientoy.R
@@ -18,10 +18,11 @@ foo <- merge(geograd, ltt)
 
 forcingnograd <- data.frame(day = 1:length(40:60), loc1 = c(40:60), loc2 = c(40:60), loc3 = c(40:60)) %>%
   pivot_longer(contains("loc"), names_to = "location", values_to = "forcing")
-forcingpositive <- data.frame(day =1:length(40:60), loc1 = c(40:60), loc2 = c(50:70), loc3 = c(60:80)) %>%
+forcingpositive <- data.frame(day =1:length(40:60), loc1 = c(40:60), loc2 = c(45:65), loc3 = c(55:75)) %>%
   pivot_longer(contains("loc"), names_to = "location", values_to = "forcing")
 
-forcingnegative <-
+forcingnegative <- data.frame(day = 1:length(40:60), loc1 = c(40:60), loc2 = c(50:70), loc3 = c(60:80)) %>%
+  pivot_longer(contains("loc"), names_to = "location", values_to = "forcing")
 
 nograd <- left_join(ltt, forcingnograd)
 plusgrad <- left_join(ltt, forcingpositive)
@@ -34,3 +35,4 @@ ggplot(plusgrad, aes(x = location, y = day, colour = type, group=location)) +
   geom_point() +
   facet_wrap("type")
 
+# this assumes all provenances at all locations
