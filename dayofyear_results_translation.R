55b307fe2ba971c89f91b380c2506c02982f3c19
Susannah <susannah@pobox.com>
2023-08-01 17:14:30 -0700

    play around with visualizing forcing in a typical year

--- dayofyear_results_translation.R
+++ dayofyear_results_translation.R
@@ -4,29 +4,32 @@
 typical_year_forc <- read.csv("data/typical_year_forc.csv") %>% # from temp mean at each site across 1945-2012
   mutate(Date = as.Date(Date_scale)) %>% select(-Date_scale)
 
-fpred_allprovs <- readRDS("objects/fepred_allprovs.rds")
+fpred_allsites <- readRDS("objects/fepred_allsites.rds")
 
-# Calculate the maximum and minimum of MAT
-max_MAT <- max(fepred_allprovs$MAT, na.rm = TRUE)
-min_MAT <- min(fepred_allprovs$MAT, na.rm = TRUE)
 
 # Filter rows where MAT is the maximum or minimum, and add a new column
-minmaxMAT_fepred <- fepred_allprovs %>%
+minmaxMAT_fepred <- fepred_allsites %>%
   # keep only coldest and warmest provenances and label
-  filter(MAT == max_MAT | MAT == min_MAT) %>%
-  mutate(size = case_when(
-    MAT == max_MAT ~ "max",
-    MAT == min_MAT ~ "min"
-  )) %>%
+  filter(Site %in% c("Kalamalka", "Border")) %>%
   # order model samples
-  group_by(model, Sex, event, size) %>%
-  arrange(Sex, event, size, .epred) %>%
-  group_by(model, size) %>%
+  group_by(model, Sex, event, Site) %>%
+  arrange(Sex, event, Site, .epred) %>%
   mutate(order = 1:n()) %>%
   select(-.row, -.draw, -MAT) %>%
-  pivot_wider(names_from = size, values_from = .epred) %>%
+  pivot_wider(names_from = Site, values_from = .epred) %>%
   # get differences
-  mutate(differences = max - min) %>%
-  group_by(Sex, event) %>%
+  mutate(differences = Kalamalka - Border) %>%
+  group_by(Sex, event)
+
+minmaxMAT_fepred_summary <- minmaxMAT_fepred %>%
   mean_hdci(differences)
-minmaxMAT_fepred
+
+# now match minmaxMAT_fepred$differences with typical_year_forc$forcing
+
+typical_year_forc_BK <- filter(typical_year_forc, Site %in% c("Border", "Kalamalka"))
+
+fepred_Kal <- filter(minmaxMAT_fepred, Site == "Kalamalka")
+
+ggplot(filter(typical_year_forc_BK, DoY > 121 & forcing > 0), aes(x = forcing, fill = Site)) +
+  geom_histogram() +
+  facet_wrap("Site")
